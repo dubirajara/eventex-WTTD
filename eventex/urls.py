aa91cfea07201c146455e40c7448b63ef1f853b5
Diego Ubirajara <diego.ubirajara@mail.com>
2017-12-10 14:10:07 +0100

    update urls change conf to path django 2.0 new feature

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.contrib import admin
 
 from eventex.core import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^inscricao/', include('eventex.subscriptions.urls',
+    path('', views.home, name='home'),
+    path('inscricao/', include('eventex.subscriptions.urls',
                                 namespace='subscriptions')),
-    url(r'^palestras/$', views.talk_list, name='talk_list'),
-    url(r'^palestrantes/(?P<slug>[\w-]+)/$', views.speaker_detail, name='speaker_detail'),
-    url(r'^admin/', admin.site.urls),
+    path('palestras/', views.talk_list, name='talk_list'),
+    path('palestrantes/<slug:slug>/', views.speaker_detail, name='speaker_detail'),
+    path('admin/', admin.site.urls),
 ]
