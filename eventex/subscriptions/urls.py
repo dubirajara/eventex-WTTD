673ae9c215bb5a9f85b227d750b2d6b1999baeb1
Diego Ubirajara <diego.ubirajara@mail.com>
2017-12-10 12:05:30 +0100

    update app_name url to work namespace django 2.0

--- eventex/subscriptions/urls.py
+++ eventex/subscriptions/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from eventex.subscriptions.views import new, detail
 
+app_name = 'subscriptions'
+
 urlpatterns = [
     url(r'^$', new, name='new'),
     url(r'^(\d+)/$', detail, name='detail'),
